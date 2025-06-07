1e8e2f2bdb4bacac46e5f5bff7792f81c2b5455a
zackel-baz <na19257@bristol.ac.uk>
2025-06-07 23:12:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
