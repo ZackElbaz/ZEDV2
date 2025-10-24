de141347776f0070e6c90dd02bbf5381fd0f1cd5
zackel-baz <na19257@bristol.ac.uk>
2025-10-24 15:11:48 +0100

    Custom Domain

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -417,5 +417,5 @@ export default HomePage;
 
 // npm run build && npm run deploy
 
-// git add . && git commit -m "Voronoi? Voroknow? Voronothing?" && git push origin main && npm run build && npm run deploy
+// git add . && git commit -m "Custom Domain" && git push origin main && npm run build && npm run deploy
 
